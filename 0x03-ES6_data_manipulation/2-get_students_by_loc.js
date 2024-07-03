6fa14dfe62b9388de57ccdc041fb8d668c552138
Emmanuel Nwachukwu <emmax0121@gmail.com>
2024-07-03 11:43:27 +0000

    array of objects

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,5 @@
-export default function getStudentsByLocation(stdList, city) {
+import getListStudents from './0-get_list_students';
+
+export default function getStudentsByLocation(stdList = getListStudents(), city) {
   return stdList.filter((studentByCity) => studentByCity.location === city);
 }
